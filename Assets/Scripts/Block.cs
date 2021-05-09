c628f9d06eca628271fc5262f4d398910c281026
Man Patel <manpatel0009@gmail.com>
2021-05-09 18:47:21 -0400

    My First Unity Game With C#

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    //config params
+    [SerializeField] AudioClip breakSound;
+    [SerializeField] GameObject blockSparklesVFX;
+    [SerializeField] Sprite[] hitSprites;
+    /*[SerializeField] int maxHits;*/
+
+    //cached reference
+    Level level;
+
+    //state variables
+    [SerializeField] int timesHit;
+
+    public void Start()
+    {
+        CountBreakableBlocks();
+    }
+
+    //
+    private void CountBreakableBlocks()
+    {
+        level = FindObjectOfType<Level>();
+        if (tag == "Breakable")
+        {
+            level.CountBlocks();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (tag == "Breakable")
+        {
+            
+            HandleHit();
+        }
+    }
+    private void HandleHit()
+    {
+        timesHit++;
+        int maxHits = hitSprites.Length + 1;
+        if (timesHit >= maxHits)
+        {
+            DestroyBlock();
+        }
+        else
+        {
+            ShowNextHitSprite();
+        }
+    }
+
+    //To show damage effect when ball hit the block 
+    private void ShowNextHitSprite()
+    {
+        int spriteIndex = timesHit - 1;
+        if(hitSprites[spriteIndex] != null)
+        {
+            GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+        }
+        else
+        {
+            Debug.LogError("Block sprite missing" + gameObject.name);
+        }
+
+    }
+
+    private void DestroyBlock()
+    {
+        PlayBlockDestroySFX();
+        Destroy(gameObject);
+        level.BlockDestroyed();
+        TriggerSparklesVFX();
+    }
+
+    //Play sound when block is destroyed
+    private void PlayBlockDestroySFX()
+    {
+        FindObjectOfType<GameStatus>().AddToScore();
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+    }
+
+    //Creates animation when ball hit the blocks
+    private void TriggerSparklesVFX()
+    {
+        GameObject sparkles = Instantiate(blockSparklesVFX, transform.position, transform.rotation);
+        Destroy(sparkles, 1f);
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+    //To load next scene when the button is clicked
+	public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+        FindObjectOfType<GameStatus>().ResetGame();
+    }
+
+    //To quit game
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
